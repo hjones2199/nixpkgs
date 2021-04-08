ad3c67f2e2b5e85fb8c2957337c0d7142621b890
Hunter Jones <hjones2199@gmail.com>
2021-04-08 01:15:22 -0500

    boost: remove old versions

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -13718,24 +13718,24 @@ in
 
   boolstuff = callPackage ../development/libraries/boolstuff { };
 
-  boost155 = callPackage ../development/libraries/boost/1.55.nix { };
-  boost159 = callPackage ../development/libraries/boost/1.59.nix { };
-  boost15x = boost159;
-  boost160 = callPackage ../development/libraries/boost/1.60.nix { };
-  boost165 = callPackage ../development/libraries/boost/1.65.nix { };
-  boost166 = callPackage ../development/libraries/boost/1.66.nix { };
-  boost167 = callPackage ../development/libraries/boost/1.67.nix { };
-  boost168 = callPackage ../development/libraries/boost/1.68.nix { };
-  boost169 = callPackage ../development/libraries/boost/1.69.nix { };
-  boost16x = boost169;
-  boost170 = callPackage ../development/libraries/boost/1.70.nix { };
-  boost171 = callPackage ../development/libraries/boost/1.71.nix { };
-  boost172 = callPackage ../development/libraries/boost/1.72.nix { };
-  boost173 = callPackage ../development/libraries/boost/1.73.nix { };
-  boost174 = callPackage ../development/libraries/boost/1.74.nix { };
+  # boost155 = callPackage ../development/libraries/boost/1.55.nix { };
+  # boost159 = callPackage ../development/libraries/boost/1.59.nix { };
+  # boost15x = boost159;
+  # boost160 = callPackage ../development/libraries/boost/1.60.nix { };
+  # boost165 = callPackage ../development/libraries/boost/1.65.nix { };
+  # boost166 = callPackage ../development/libraries/boost/1.66.nix { };
+  # boost167 = callPackage ../development/libraries/boost/1.67.nix { };
+  # boost168 = callPackage ../development/libraries/boost/1.68.nix { };
+  # boost169 = callPackage ../development/libraries/boost/1.69.nix { };
+  # boost16x = boost169;
+  # boost170 = callPackage ../development/libraries/boost/1.70.nix { };
+  # boost171 = callPackage ../development/libraries/boost/1.71.nix { };
+  # boost172 = callPackage ../development/libraries/boost/1.72.nix { };
+  # boost173 = callPackage ../development/libraries/boost/1.73.nix { };
+  # boost174 = callPackage ../development/libraries/boost/1.74.nix { };
   boost175 = callPackage ../development/libraries/boost/1.75.nix { };
   boost17x = boost175;
-  boost = boost16x;
+  boost = boost17x;
 
   boost_process = callPackage ../development/libraries/boost-process { };
 
